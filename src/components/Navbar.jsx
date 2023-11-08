d3d3c6f65997dab0a8e690d29a3cd9aeb02f52ba
Melisa Şahin <mmelisasahin@gmail.com>
2023-11-09 00:25:50 +0300

    updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -31,7 +31,7 @@ const Navbar = () =>
     });
 
     return ( 
-       <header className='w-full bg-white md:bg-transparent fixed top-0 left-0 right-0 text-black'>
+       <header className='w-full  md:bg-transparent fixed top-0 left-0 right-0 text-black'>
         <nav className={`py-8 lg:px-14 px-4 m-2 rounded-3xl ${isSticky ? "sticky top-0 left-0 right-0 border bg-white duration-300" :""}`}>
             <div className='flex justify-between items-center text-xl gap-8'>
                 <a href='/' className='text-3xl font-semibold flex items-center space-x-3'><img src={logo} alt="" className='w-10 inline-block items-center' /><span className='text-[#263238]'>XXXX</span></a>
@@ -63,12 +63,12 @@ const Navbar = () =>
                 </div>   
             </div>   
             <div className={`space-y-4 px-4 py-10 bg-white flex flex-col items-center h-[450px] mt-[114px] text-base  ${isMenuOpen ? "block fixed left-0 right-0 top-0" : "hidden"}`}>
-                    
+                <ul className='text-center gap-4 text-lg'> 
                     <li><Link to="/">Nasıl Çalışır</Link></li>
                     <li><Link to="/">Hakkımızda</Link></li>
                     <li><Link to="/psikolog">Psikolog</Link></li>
                     <li><Link to="/sss">SSS</Link></li>
-               
+                </ul>   
                 <div>
                     <button className='lg:flex items-center text-brandPrimary hover:text-gray900' onClick={() => setOpenModel(true)} >Login</button>
                     {openModel && <LoginModel closeModal={setOpenModel} /> }
